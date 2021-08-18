a47101e418c04e215be911205309fd1701f00843
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-08-18 14:45:34 +0530

    FX stage -1

--- src/main/java/lk/ijse/dep7/controller/ManageCustomersFormController.java
+++ src/main/java/lk/ijse/dep7/controller/ManageCustomersFormController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
@@ -24,17 +25,30 @@ public class ManageCustomersFormController {
     public JFXTextField txtCustomerId;
     public JFXButton btnDelete;
     public JFXButton btnSave;
+    public JFXButton btnAddNewCustomer;
     public JFXTextField txtCustomerAddress;
     public TableView<CustomerTM> tblCustomers;
 
-    public void initialize(){
+    public void initialize() {
         tblCustomers.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
         tblCustomers.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
         tblCustomers.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
 
-        txtCustomerId.setEditable(false);
-        btnSave.setDisable(true);
-        btnDelete.setDisable(true);
+        initUI();
+
+        tblCustomers.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            btnDelete.setDisable(newValue == null);
+
+            if (newValue != null) {
+                txtCustomerId.setText(newValue.getId());
+                txtCustomerName.setText(newValue.getName());
+                txtCustomerAddress.setText(newValue.getAddress());
+
+                txtCustomerId.setDisable(false);
+                txtCustomerName.setDisable(false);
+                txtCustomerAddress.setDisable(false);
+            }
+        });
     }
 
     @FXML
@@ -49,26 +63,66 @@ public class ManageCustomersFormController {
     }
 
     public void btnAddNew_OnAction(ActionEvent actionEvent) {
+        txtCustomerId.setDisable(false);
+        txtCustomerName.setDisable(false);
+        txtCustomerAddress.setDisable(false);
+
         txtCustomerId.clear();
         txtCustomerId.setText(generateNewId());
         txtCustomerName.clear();
         txtCustomerAddress.clear();
         txtCustomerName.requestFocus();
         btnSave.setDisable(false);
+
+        tblCustomers.getSelectionModel().clearSelection();
     }
 
     public void btnSave_OnAction(ActionEvent actionEvent) {
+        String id = txtCustomerId.getText();
+        String name = txtCustomerName.getText();
+        String address = txtCustomerAddress.getText();
+
+        if (!name.matches("[A-Za-z ]+")) {
+            new Alert(Alert.AlertType.ERROR, "Invalid name").show();
+            txtCustomerName.requestFocus();
+            return;
+        } else if (!address.matches(".{3,}")) {
+            new Alert(Alert.AlertType.ERROR, "Address should be least 3 character long").show();
+            txtCustomerAddress.requestFocus();
+            return;
+        }
+
+        //Todo: save this in our DB first
+        tblCustomers.getItems().add(new CustomerTM(id, name, address));
+        btnAddNewCustomer.fire();
     }
 
     public void btnDelete_OnAction(ActionEvent actionEvent) {
+        tblCustomers.getItems().remove(tblCustomers.getSelectionModel().getSelectedItem());
+        tblCustomers.getSelectionModel().clearSelection();
+        initUI();
     }
 
-    private String generateNewId(){
+    private String generateNewId() {
         if (tblCustomers.getItems().isEmpty()) {
             return "C001";
-        }else {
-            // Todo: generate new id
-            throw new UnsupportedOperationException("Yet to implement");
+        } else {
+            String id = tblCustomers.getItems().get(tblCustomers.getItems().size() - 1).getId();
+            int newCustomerId = Integer.parseInt(id.replace("C", "")) + 1;
+            return String.format("C%03d", newCustomerId);
         }
+    }
+
+    private void initUI() {
+        txtCustomerId.clear();
+        txtCustomerName.clear();
+        txtCustomerAddress.clear();
+
+        txtCustomerId.setDisable(true);
+        txtCustomerName.setDisable(true);
+        txtCustomerAddress.setDisable(true);
+        txtCustomerId.setEditable(false);
+        btnSave.setDisable(true);
+        btnDelete.setDisable(true);
     }
 }
---
+++ src/test/java/lk/ijse/dep7/controller/ManageCustomersFormControllerTest.java
@@ -0,0 +1,14 @@
+package lk.ijse.dep7.controller;
+
+import org.junit.jupiter.api.Test;
+
+class ManageCustomersFormControllerTest {
+
+    @Test
+    void generateNewId2() {
+        ManageCustomersFormController ctrl = new ManageCustomersFormController();
+        /*assertEquals(ctrl.generateNewId2("C001"),"C002");
+        assertEquals(ctrl.generateNewId2("C010"),"C011");
+        assertEquals(ctrl.generateNewId2("C1000"),"C1001");*/
+    }
+}
