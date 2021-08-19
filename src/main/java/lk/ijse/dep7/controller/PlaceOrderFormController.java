7c16a34e1f7216ffdae0813d043c309068ca958e
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-08-19 17:22:45 +0530

    Load item ids  and names

--- src/main/java/lk/ijse/dep7/controller/PlaceOrderFormController.java
+++ src/main/java/lk/ijse/dep7/controller/PlaceOrderFormController.java
@@ -16,9 +16,11 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import lk.ijse.dep7.dbUtils.SingleConnectionDataSource;
+import lk.ijse.dep7.dto.ItemDTO;
 import lk.ijse.dep7.exception.FailedOperationException;
 import lk.ijse.dep7.exception.NotFoundException;
 import lk.ijse.dep7.service.CustomerService;
+import lk.ijse.dep7.service.ItemService;
 
 import java.io.IOException;
 import java.net.URL;
@@ -34,7 +36,10 @@ public class PlaceOrderFormController {
     public JFXButton btnSave;
     public TableView tblOrderDetails;
     public JFXTextField txtUnitPrice;
+
     private final CustomerService customerService = new CustomerService(SingleConnectionDataSource.getInstance().getConnection());
+    private final ItemService itemService = new ItemService(SingleConnectionDataSource.getInstance().getConnection());
+
     public JFXComboBox<String> cmbCustomerId;
     public JFXTextField txtQty;
     public Label lblId;
@@ -44,9 +49,11 @@ public class PlaceOrderFormController {
 
     public void initialize() throws FailedOperationException {
         //Todo: we need to generate and set new order id
+
         lblDate.setText(LocalDate.now().toString());
         btnPlaceOrder.setDisable(true);
         txtCustomerName.setEditable(false);
+
         cmbCustomerId.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue != null) {
                 try {
@@ -54,18 +61,46 @@ public class PlaceOrderFormController {
                 } catch (NotFoundException e) {
                     e.printStackTrace(); //This can't be happened with our UI
                 } catch (FailedOperationException e) {
+                    new Alert(Alert.AlertType.ERROR, "Failed to load customer information").show();
                     throw new RuntimeException(e);
                 }
             }
         });
-        loadAllCustomers();
+
+        cmbItemCode.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                try {
+                    ItemDTO item = itemService.findItem(newValue);
+                    txtDescription.setText(item.getDescription());
+                    txtUnitPrice.setText(item.getUnitPrice().setScale(2).toString());
+                    txtQty.setText(String.valueOf(item.getQtyOnHand()));
+                } catch (NotFoundException e) {
+                    e.printStackTrace();
+                } catch (FailedOperationException e) {
+                    new Alert(Alert.AlertType.ERROR, "Failed to load item information").show();
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        loadAllCustomersIds();
+        loadAllItemCodes();
     }
 
-    private void loadAllCustomers() throws FailedOperationException {
+    private void loadAllCustomersIds() throws FailedOperationException {
         try {
             customerService.findAllCustomers().forEach(dto -> cmbCustomerId.getItems().add(dto.getId()));
         } catch (FailedOperationException e) {
             new Alert(Alert.AlertType.ERROR, "Failed to load customer ids").show();
+            throw e;
+        }
+    }
+
+    private void loadAllItemCodes() throws FailedOperationException {
+        try {
+            itemService.findAllItems().forEach(dto -> cmbItemCode.getItems().add(dto.getCode()));
+        } catch (FailedOperationException e) {
+            new Alert(Alert.AlertType.ERROR, "Failed to load item codes").show();
             throw e;
         }
     }
