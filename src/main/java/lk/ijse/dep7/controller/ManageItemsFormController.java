195f03f08de8084217fde7d62516b6b947606c20
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-08-19 13:57:46 +0530

    Add ItemTM

--- src/main/java/lk/ijse/dep7/controller/ManageItemsFormController.java
+++ src/main/java/lk/ijse/dep7/controller/ManageItemsFormController.java
@@ -9,9 +9,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.dep7.util.ItemTM;
 
 import java.io.IOException;
 import java.net.URL;
@@ -24,8 +26,15 @@ public class ManageItemsFormController {
     public JFXTextField txtQtyOnHand;
     public JFXButton btnDelete;
     public JFXButton btnSave;
-    public TableView tblItems;
+    public TableView<ItemTM> tblItems;
     public JFXTextField txtUnitPrice;
+
+    public void initialize() {
+        tblItems.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("code"));
+        tblItems.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("description"));
+        tblItems.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("qtyOnHand"));
+        tblItems.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+    }
 
     @FXML
     private void navigateToHome(MouseEvent event) throws IOException {
@@ -35,7 +44,7 @@ public class ManageItemsFormController {
         Stage primaryStage = (Stage) (this.root.getScene().getWindow());
         primaryStage.setScene(scene);
         primaryStage.centerOnScreen();
-        Platform.runLater(()->primaryStage.sizeToScene());
+        Platform.runLater(primaryStage::sizeToScene);
     }
 
     public void btnAddNew_OnAction(ActionEvent actionEvent) {
