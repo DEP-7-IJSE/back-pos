560033c22f841ef1273f0f70c54c1ca86e1eba97
Pethum-Jeewantha <pethumjeewantha1@gmail.com>
2021-08-19 17:50:49 +0530

    Add new order detail to the view

--- src/main/java/lk/ijse/dep7/controller/PlaceOrderFormController.java
+++ src/main/java/lk/ijse/dep7/controller/PlaceOrderFormController.java
@@ -4,14 +4,14 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import javafx.application.Platform;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -21,8 +21,10 @@ import lk.ijse.dep7.exception.FailedOperationException;
 import lk.ijse.dep7.exception.NotFoundException;
 import lk.ijse.dep7.service.CustomerService;
 import lk.ijse.dep7.service.ItemService;
+import lk.ijse.dep7.util.OrderDetailTM;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.URL;
 import java.time.LocalDate;
 
@@ -34,7 +36,7 @@ public class PlaceOrderFormController {
     public JFXTextField txtDescription;
     public JFXTextField txtQtyOnHand;
     public JFXButton btnSave;
-    public TableView tblOrderDetails;
+    public TableView<OrderDetailTM> tblOrderDetails;
     public JFXTextField txtUnitPrice;
 
     private final CustomerService customerService = new CustomerService(SingleConnectionDataSource.getInstance().getConnection());
@@ -48,11 +50,26 @@ public class PlaceOrderFormController {
     public JFXComboBox<String> cmbItemCode;
 
     public void initialize() throws FailedOperationException {
+        tblOrderDetails.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("code"));
+        tblOrderDetails.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("description"));
+        tblOrderDetails.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("qty"));
+        tblOrderDetails.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+        tblOrderDetails.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("total"));
+        TableColumn<OrderDetailTM, Button> lastCol = (TableColumn<OrderDetailTM, Button>) tblOrderDetails.getColumns().get(5);
+        lastCol.setCellValueFactory(param -> {
+            Button btnDelete = new Button("Delete");
+
+            return new ReadOnlyObjectWrapper<>(btnDelete);
+        });
+
         //Todo: we need to generate and set new order id
 
         lblDate.setText(LocalDate.now().toString());
         btnPlaceOrder.setDisable(true);
         txtCustomerName.setEditable(false);
+        txtQty.setOnAction(event -> btnSave.fire());
+        txtQty.setEditable(false);
+        btnSave.setDisable(true);
 
         cmbCustomerId.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue != null) {
@@ -68,12 +85,15 @@ public class PlaceOrderFormController {
         });
 
         cmbItemCode.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            txtQty.setEditable(newValue != null);
+            btnSave.setDisable(newValue == null);
+
             if (newValue != null) {
                 try {
                     ItemDTO item = itemService.findItem(newValue);
                     txtDescription.setText(item.getDescription());
                     txtUnitPrice.setText(item.getUnitPrice().setScale(2).toString());
-                    txtQty.setText(String.valueOf(item.getQtyOnHand()));
+                    txtQtyOnHand.setText(String.valueOf(item.getQtyOnHand()));
                 } catch (NotFoundException e) {
                     e.printStackTrace();
                 } catch (FailedOperationException e) {
@@ -117,6 +137,20 @@ public class PlaceOrderFormController {
     }
 
     public void btnAdd_OnAction(ActionEvent actionEvent) {
+        if (!txtQty.getText().matches("\\d+}") || Integer.parseInt(txtQty.getText()) <= 0 ||
+                Integer.parseInt(txtQty.getText()) > Integer.parseInt(txtQtyOnHand.getText())) {
+            new Alert(Alert.AlertType.ERROR, "Invalid Qty").show();
+            txtQty.requestFocus();
+            txtQty.selectAll();
+            return;
+        }
+        String itemCode = cmbItemCode.getSelectionModel().getSelectedItem();
+        String description = txtDescription.getText();
+        BigDecimal unitPrice = new BigDecimal(txtUnitPrice.getText()).setScale(2);
+        int qty = Integer.parseInt(txtQty.getText());
+        BigDecimal total = unitPrice.multiply(new BigDecimal(qty)).setScale(2);
+
+        tblOrderDetails.getItems().add(new OrderDetailTM(itemCode, description, qty, unitPrice, total));
     }
 
     public void txtQty_OnAction(ActionEvent actionEvent) {
